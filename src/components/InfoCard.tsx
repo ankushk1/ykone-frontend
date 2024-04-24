157861e054cdd781ff672d48bce7d65647816cb9
Ankush <ankushkapoor179@gmail.com>
2024-04-24 16:51:13 +0530

    replaced image icons

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -2,12 +2,7 @@ import { Home, Mail } from "react-feather";
 import { useNavigate } from "react-router-dom";
 import { IClient } from "../utils/interfaces";
 
-const InfoCard = ({
-  _id,
-  name,
-  email,
-  address: {  address }
-}: IClient) => {
+const InfoCard = ({ _id, name, email, address: { address } }: IClient) => {
   const navigate = useNavigate();
 
   const handleRedirect = () => {
@@ -17,21 +12,28 @@ const InfoCard = ({
   return (
     <div>
       <div className="w-[465px] h-[350px] flex flex-col justify-between bg-white rounded-2xl shadow">
-        <div className="px-6 py-8 sm:p-10 sm:pb-6">
-          <div className="flex flex-col items-center justify-center">
+        <div className="px-5 py-8 sm:p-10 sm:pb-6">
+          <div className="flex flex-col items-center">
             <div>
               <h2 className="text-2xl tracking-tighter text-gray-800">
                 {name}
               </h2>
-              <p className=" flex items-start  mt-2 text-md text-gray-700">
-                <Mail className="inline w-4 h-8 mr-2 pb-[6px] aspect-square" />
+              <p className=" flex items-start gap-2 text-md text-gray-700">
+                <img
+                  src="/icons/mail.svg"
+                  alt="home"
+                  className="w-4 aspect-square mt-1"
+                />
                 {email}
               </p>
             </div>
             <div>
-              <p className="flex items-start mt-2 text-sm text-gray-700">
-                {" "}
-                <Home className="inline w-9 aspect-square pb-[6px] mr-2" />
+              <p className="flex items-start text-sm text-gray-700 gap-2">
+                <img
+                  src="/icons/home.svg"
+                  alt="home"
+                  className="w-4 aspect-square mt-0.5"
+                />
                 {address}
               </p>
             </div>
